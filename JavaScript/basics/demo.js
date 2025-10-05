f71981115ef2b456cb9acd0716d9a39d74bd40ba
Govindh Ramanan <govindhramanan@gmail.com>
2025-10-05 20:33:30 +0530

    Initial commit of Web Tech project

---
+++ JavaScript/basics/demo.js
@@ -0,0 +1,25 @@
+let n;
+console.log(n)
+n=10;
+console.log(n)
+n="Hi Govindh Ramanan"
+console.log(n)
+n=true
+console.log(n)
+
+
+
+console.log("Can be printed as same as in python directly")
+
+
+function main(){
+    console.log("Function side printing")
+}
+main()
+
+class A{
+     static main(){
+        console.log("Like Java printing it is case sensitive too")
+     }
+}
+A.main()
